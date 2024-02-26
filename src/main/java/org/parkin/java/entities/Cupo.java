abbcab752cc8f6c8b162943b93a842558073ed7a
samuel <bayonas067@gmail.com>
2024-02-26 14:25:14 -0500

    entidades listas para el main

--- src/main/java/org/parkin/java/entities/Cupo.java
+++ src/main/java/org/parkin/java/entities/Cupo.java
@@ -1,5 +1,24 @@
 package org.parkin.java.entities;
 
 public class Cupo {
+    public char nombre;
+
+    
+
+
+//constructor por defecto
+public Cupo() {
+    }
+
+
+
+// constructor parametrizado
+public Cupo(char nombre) {
+    this.nombre = nombre;
+}
+
+
+
+
 
 }
--- src/main/java/org/parkin/java/entities/Pago.java
+++ src/main/java/org/parkin/java/entities/Pago.java
@@ -1,5 +1,35 @@
 package org.parkin.java.entities;
+import java.time.LocalDateTime;
 
 public class Pago {
+    //atributos intrinsecos
+    public LocalDateTime fechaInicio;
+    public LocalDateTime fechaHoraFin;
+    public double valor;
+    //atributos de relacion
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+    
+    
+    
+    // constructor por defecto
+    public Pago() {
+
+    }
+
+    // constructor parametrizado
+
+    public Pago(LocalDateTime fechaInicio, 
+                LocalDateTime fechaHoraFin,
+                double valor, 
+                Vehiculo vehiculo, 
+                Cupo cupo) {
+        this.fechaInicio = fechaInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+    
 
 }
